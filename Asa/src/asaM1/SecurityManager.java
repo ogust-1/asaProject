d9a7f85353d625f70abcab22f542f3996d54ee82
ogust-1 <73693613+ogust-1@users.noreply.github.com>
2021-12-06 14:38:01 +0100

    ajout de diverses choses

--- Asa/src/asaM1/SecurityManager.java
+++ Asa/src/asaM1/SecurityManager.java
@@ -1,5 +1,26 @@
 package asaM1;
 
-public class SecurityManager {
+import asa.AsaFactory;
+import asa.AsaPackage;
+import asa.PortFournisComposant;
+import asa.PortRequisComposant;
+import asa.impl.ComposantImpl;
+
+public class SecurityManager extends ComposantImpl {
+	
+	AsaPackage aPackage=AsaPackage.eINSTANCE;
+	AsaFactory factory= aPackage.getAsaFactory();
+	
+	private PortRequisComposant in;
+	private PortFournisComposant out;
+	
+	public SecurityManager() {
+		super();
+		this.setName("SecurityManager");
+		
+		in=factory.createPortRequisComposant();
+		out=factory.createPortFournisComposant();
+	}
+	
 
 }
